503b8e9365f76093c47a4f7b7c7cd076cc5170f1
AlisonAlmeida <alison.asa@gmail.com>
2022-11-30 15:32:56 -0400

    C

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,10 +3,13 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:pokeapi/model/pokemon/gender.dart';
+import 'package:pokeapi/model/pokemon/pokemon-specie.dart';
 import 'package:pokeapi/model/pokemon/pokemon.dart';
+import 'package:pokeapi/model/utils/common.dart';
 import 'package:pokeapi/pokeapi.dart';
-import 'package:pokedex/main.dart';
 import 'package:pokedex/utils/colors.dart';
+import 'package:lottie/lottie.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,7 +19,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Pokemon? pokemon;
+  bool isInitialized = false;
+
   int maxPokemonNumber = 151;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future pickPokemon() async {
+    NamedAPIResource namedAPIResource;
+    Random random = Random();
+    pokemon = await PokeAPI.getObject(random.nextInt(maxPokemonNumber + 1));
+    namedAPIResource = pokemon!.species!;
+
+    PokemonSpecie pokemonSpecie = PokemonSpecie();
+    //AASSSISTE ALGUMA AULA DE POKEAPI NA NET
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,58 +60,96 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
           body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: ListView(
-              children: [
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        child: Text(
-                          pokemon!.name == ''
-                              ? '-'
-                              : pokemon!.name!.toUpperCase(),
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w700),
+              padding: const EdgeInsets.all(20),
+              child: FutureBuilder(
+                future: pickPokemon(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting ||
+                      snapshot.connectionState == ConnectionState.none) {
+                    return Container(
+                      color: Colors.grey[300],
+                      child: Center(
+                          child: SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Lottie.asset(
+                            'lib/assets/animations/pokeball-animation.json'),
+                      )),
+                    );
+                  } else if (snapshot.connectionState == ConnectionState.done) {
+                    return ListView(
+                      children: [
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  pokemon!.name == ''
+                                      ? '-'
+                                      : pokemon!.name!.toUpperCase(),
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                height: 40,
+                                decoration: screenDecoration,
+                              ),
+                            ),
+                          ],
                         ),
-                        height: 40,
-                        decoration: screenDecoration,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        child: Image.network(pokemon!.sprites!.frontDefault!,
-                            fit: BoxFit.fill),
-                        height: 250,
-                        decoration: screenDecoration,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                        child: Container(
-                      child: Text(
-                        '  #${pokemon!.id} - ${typeNames!}',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      decoration: screenDecoration,
-                    ))
-                  ],
-                )
-              ],
-            ),
-          ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                child: Image.network(
+                                    pokemon!.sprites!.frontDefault!,
+                                    fit: BoxFit.fitHeight),
+                                height: 250,
+                                decoration: screenDecoration,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Expanded(
+                                child: Container(
+                              child: Text(
+                                '  #${pokemon!.id} - Altura: ${pokemon!.height! * 2.54} cm - Peso: ${pokemon!.weight! * 453 / 1000} kg',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              decoration: screenDecoration,
+                            ))
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Expanded(
+                                child: Container(
+                              child: Text(
+                                pokemon!.forms.toString(),
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              decoration: screenDecoration,
+                            ))
+                          ],
+                        ),
+                      ],
+                    );
+                  } else {
+                    return Center(
+                      child: Text('ERRO'),
+                    );
+                  }
+                },
+              )),
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.black,
@@ -99,11 +158,7 @@ class _HomePageState extends State<HomePage> {
             child: Image.asset('lib/assets/img/pokebola.png'),
           ),
           onPressed: () async {
-            Random random = Random();
-
-            pokemon =
-                await PokeAPI.getObject(random.nextInt(maxPokemonNumber + 1));
-
+            await pickPokemon();
             setState(() {});
           },
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:pokeapi/model/pokemon/pokemon.dart';
-
-import 'package:pokeapi/pokeapi.dart';
 import 'package:pokedex/home_page.dart';
 
-late Pokemon? pokemon;
-late List<Types>? types;
-List<String>? typeNames = [];
+
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  pokemon = await PokeAPI.getObject(1);
-  types = pokemon!.types;
-  for (var type in types!) {
-    typeNames!.add(type.type!.name!);
-  }
+
 
   runApp(const MyApp());
 }
