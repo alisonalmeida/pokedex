030565bc79b83d7e3829fad7ce5b8095efd3cb3f
AlisonAlmeida <alison.asa@gmail.com>
2023-03-10 13:43:49 -0400

    x

--- lib/components/pokemon_grid_tile.dart
+++ lib/components/pokemon_grid_tile.dart
@@ -8,25 +8,28 @@ class PokemonTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(border: Border.all()),
-      child: GridTile(
-        footer: Text(
-          pokemon.name[0].toUpperCase() +
-              pokemon.name.substring(1, pokemon.name.length),
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.asset('lib/assets/pokemon_gif/${pokemon.id}front.gif'),
+        Container(
+          margin: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(25))),
+        ),
+        Positioned(top: 1, left: 1, child: PokemonNumber(pokemon: pokemon)),
+        Positioned(
+          bottom: 10,
+          left: 10,
+          child: Text(
+            pokemon.name[0].toUpperCase() +
+                pokemon.name.substring(1, pokemon.name.length),
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Positioned(
-                top: 10, left: 10, child: PokemonNumber(pokemon: pokemon)),
-            Image.asset('lib/assets/pokemon_gif/${pokemon.id}front.gif'),
-          ],
-        ),
-      ),
+      ],
     );
   }
 }
